000fb91111d3e65583d6d6214a4c0e07dab9c327
Gene Chan <ccl0413gene6@gmail.com>
2023-09-25 15:24:30 +0100

    inital commit

---
+++ f1_Kit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  f1_Kit
+//
+//  Created by Gin on 25/9/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
